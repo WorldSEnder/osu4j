a2628c44e2f200510910df9af2490882a2686252
WorldSEnder <worldsbegin@gmx.de>
2016-10-09 03:12:58 +0200

    Added game mods

--- src/main/java/com/github/oopsjpeg/osu4j/GameMod.java
+++ src/main/java/com/github/oopsjpeg/osu4j/GameMod.java
@@ -1,37 +1,48 @@
 package com.github.oopsjpeg.osu4j;
 
 public enum GameMod {
-
-    // None = 0,
-    // NoFail = 1,
-    // Easy = 2,
-    // //NoVideo = 4,
-    // Hidden = 8,
-    // HardRock = 16,
-    // SuddenDeath = 32,
-    // DoubleTime = 64,
-    // Relax = 128,
-    // HalfTime = 256,
-    // Nightcore = 512, // Only set along with DoubleTime. i.e: NC only gives 576
-    // Flashlight = 1024,
-    // Autoplay = 2048,
-    // SpunOut = 4096,
-    // Relax2 = 8192, // Autopilot?
-    // Perfect = 16384,
-    // Key4 = 32768,
-    // Key5 = 65536,
-    // Key6 = 131072,
-    // Key7 = 262144,
-    // Key8 = 524288,
+    NONE(0x0),
+    NO_FAIL(0x1),
+    EASY(0x2),
+    NO_VIDEO(0x4),
+    HIDDEN(0x8),
+    HARD_ROCK(0x10),
+    SUDDEN_DEATH(0x20),
+    DOUBLE_TIME(0x40),
+    RELAX(0x80),
+    HALF_TIME(0x100),
+    // Only set if double-time is set, too
+    NIGHTCORE(0x200),
+    FLASHLIGHT(0x400),
+    AUTOPLAY(0x800),
+    SPUN_OUT(0x1000),
+    KINO(0x2000),
+    PERFECT(0x4000),
+    KEY1(0x4000000),
+    KEY2(0x10000000),
+    KEY3(0x8000000),
+    KEY4(0x8000),
+    KEY5(0x10000),
+    KEY6(0x20000),
+    KEY7(0x40000),
+    KEY8(0x80000),
+    KEY9(0x1000000),
+    KEY10(0x2000000),
+    FADE_IN(0x100000),
+    RANDOM(0x200000),
+    LAST_MOD(0x400000);
     // keyMod = Key4 | Key5 | Key6 | Key7 | Key8,
-    // FadeIn = 1048576,
-    // Random = 2097152,
-    // LastMod = 4194304,
     // FreeModAllowed = NoFail | Easy | Hidden | HardRock | SuddenDeath | Flashlight | FadeIn | Relax | Relax2 | SpunOut
-    // | keyMod,
-    // Key9 = 16777216,
-    // Key10 = 33554432,
-    // Key1 = 67108864,
-    // Key3 = 134217728,
-    // Key2 = 268435456
+    // | keyMod
+    ;
+
+    private final int bitMask;
+
+    private GameMod(int mask) {
+        this.bitMask = mask;
+    }
+
+    public int getBitMask() {
+        return bitMask;
+    }
 }
--- src/main/java/com/github/oopsjpeg/osu4j/GameMods.java
+++ src/main/java/com/github/oopsjpeg/osu4j/GameMods.java
@@ -3,17 +3,30 @@ package com.github.oopsjpeg.osu4j;
 import java.util.EnumSet;
 
 public class GameMods {
+    private static GameMod[] ALL_MODS = GameMod.values();
 
     private EnumSet<GameMod> enabledMods;
 
+    private GameMods(EnumSet<GameMod> mods) {
+        this.enabledMods = mods;
+    }
+
     public static GameMods parse(int modes) {
-        // TODO:
-        return null;
+        EnumSet<GameMod> mods = EnumSet.noneOf(GameMod.class);
+        for (GameMod mod : ALL_MODS) {
+            int modMask = mod.getBitMask();
+            if ((modes & modMask) == modMask) {
+                mods.add(mod);
+            }
+        }
+        return new GameMods(mods);
     }
 
     public int asInt() {
-        // Reverse of #parse
-        // TODO Auto-generated method stub
-        return 0;
+        int i = 0;
+        for (GameMod mod : enabledMods) {
+            i &= mod.getBitMask();
+        }
+        return i;
     }
 }
